a86b0de02ad91fa115dad4deec44cfbb86d2e24a
jamesluc007 <lucianobruna007@gmail.com>
2021-06-13 19:46:54 -0300

    Improved coordinates system

--- darp_analyzer_webapp.py
+++ darp_analyzer_webapp.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-from utils.analyzer_utils import load_data, update_data, query_city_coordenates, get_city_coordinates
+from utils.analyzer_utils import load_data, update_data, get_capital_city, query_city_coordinates, get_city_coordinates
 
 # WEB APP DEFINITION
 
@@ -11,6 +11,7 @@ class WebApp():
         self.groups_df = pd.DataFrame()
         self.latencies_df = pd.DataFrame()
 
+        self._update_data()
         self.update_data_button()
         self.raw_data_visualization()
         self.map_visualization()
@@ -19,11 +20,14 @@ class WebApp():
     def update_data_button(self):
         button = st.button('Update Data')
         if button:
-            update_data()
-            groups = load_data(data_path='groups')
-            latencies = load_data(data_path='latencies')
-            self.groups_df = pd.DataFrame.from_dict(groups['nodes'])
-            self.latencies_df = pd.DataFrame.from_dict(latencies['matrix'])
+            self._update_data()
+
+    def _update_data(self):
+        update_data()
+        groups = load_data(data_path='groups')
+        latencies = load_data(data_path='latencies')
+        self.groups_df = pd.DataFrame.from_dict(groups['nodes'])
+        self.latencies_df = pd.DataFrame.from_dict(latencies['matrix'])
 
     def raw_data_visualization(self):
         st.header("Raw Data Visualization")
@@ -36,12 +40,14 @@ class WebApp():
         st.header("Map Visualization")
         
         df_map = pd.DataFrame()
-        for i in [x for x in self.groups_df['city']]:
-            if i is None:
-                continue
-            temp_df = get_city_coordinates(address=i)
-            if temp_df is not None:
-                df_map = df_map.append(temp_df)
+        countries_list = [x for x in self.groups_df['country']]
+        for i, city in enumerate([x for x in self.groups_df['city']]):
+
+            if city is None:
+                city = get_capital_city(countries_list[i])
+
+            temp_df = get_city_coordinates(address=str(city))
+            df_map = df_map.append(temp_df)
 
         st.write(df_map)
         st.map(df_map[['lat', 'lon']])
@@ -55,5 +61,4 @@ class WebApp():
 st.title("DARP Analyzer Webapp")
 st.write("This web app will help you to visualize and analyze your DARP data.")
 webapp = WebApp()
-
     
--- utils/analyzer_utils.py
+++ utils/analyzer_utils.py
@@ -4,6 +4,7 @@ from os import listdir
 from os.path import isfile, join
 import os
 import json
+import yaml
 import requests
 import urllib.parse
 
@@ -13,7 +14,7 @@ def load_data(data_path, id=None):
 
     basePath = os.path.dirname(os.path.abspath(__file__))
 
-    list_of_data = []
+    #list_of_data = []
     # This assumes that there is only one file in this location
     # Thus, previous data removal is necessary
     for i in data_files:
@@ -33,17 +34,19 @@ def load_data(data_path, id=None):
 def update_data():
     os.system('sh ./data/load_data.sh')
 
-def query_city_coordenates(address='Dubai'):
+def query_city_coordinates(address='Dubai'):
     url = 'https://nominatim.openstreetmap.org/search/' + urllib.parse.quote(address) +'?format=json'
     response = requests.get(url).json()
 
     if not response:
-        return None
-
-    df_cities = pd.DataFrame.from_dict(response[0])    
-    df_cities = df_cities[['lat', 'lon']]
-    df_cities = df_cities.astype(np.float16)
-    df_cities = df_cities.drop(df_cities.index[range(1,len(df_cities.index))])
+        #df_cities = pd.DataFrame()
+        # Dangerous workaround for Å iauliai:
+        df_cities = pd.DataFrame(columns=["lat", "lon", "city"], data=[[55.92,23.31, address]])
+    else:
+        df_cities = pd.DataFrame.from_dict(response[0])
+        df_cities = df_cities[['lat', 'lon']]
+        df_cities = df_cities.astype(np.float16)
+        df_cities = df_cities.drop(df_cities.index[range(1,len(df_cities.index))])
 
     return df_cities
 
@@ -51,9 +54,26 @@ def get_city_coordinates(address='Dubai'):
 
     df_cities = pd.read_csv('utils/city_coordinates.csv')  
 
+    df_cities.drop_duplicates(subset ="city",
+                     keep = 'first', inplace = True)
+
     df_cities = df_cities.loc[df_cities['city'] == address]
 
-    if df_cities is not None:
+    if not df_cities.empty:
         return df_cities
     else:
-        return query_city_coordenates(address=address)
+        temp = query_city_coordinates(address=address)
+        temp['city'] = address
+        return temp
+
+def get_capital_city(country_prefix):
+
+    with open('utils/country_prefixes.yml') as f:
+        countries_dict = yaml.safe_load(f)
+    
+    df_capitals = pd.read_csv('utils/country_list.csv')
+
+    df_capitals = df_capitals.loc[df_capitals['country'] == countries_dict[country_prefix]]
+
+    return df_capitals['capital'].iloc[0]
+
