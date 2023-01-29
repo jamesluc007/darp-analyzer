666df6b40c97d8005f0f1ced4f4ee03e47805bf2
jamesluc007 <lucianobruna007@gmail.com>
2023-01-29 19:17:59 -0300

    Improved page organization

--- darp_analyzer_webapp.py
+++ darp_analyzer_webapp.py
@@ -1,4 +1,6 @@
 import streamlit as st
+
+st.set_page_config(layout="wide")
 import pandas as pd
 import numpy as np
 
@@ -17,17 +19,24 @@ class WebApp:
 
     def __init__(self):
         # Attributes Initialization:
-        self.groups_df = pd.DataFrame()
-        self.latencies_df = pd.DataFrame()
+        if "groups_df" not in st.session_state:
+            st.session_state["groups_df"] = pd.DataFrame()
+        if "latencies_df" not in st.session_state:
+            st.session_state["latencies_df"] = pd.DataFrame()
         self.config_dict = {}
         self._update_data()
 
         # Sections Initialization:
-        st.write("These are the current running DARP nodes:")
-        st.write(self.groups_df)
-        self.configuration()
-        self.update_data_button()
-        self.execute()
+        col1, col2 = st.columns(2)
+
+        with col1:
+            st.write("These are the current running DARP nodes:")
+            st.write(st.session_state["groups_df"])
+            self.update_data_button()
+
+        with col2:
+            self.configuration()
+            self.execute()
 
     def configuration(self) -> None:
         """
@@ -44,14 +53,11 @@ class WebApp:
         if self.config_dict["checkbox_map"]:
             self.config_dict["maps_number"] = st.slider("Number of Maps", 1, 4, 1)
             st.caption("Select the number of desired maps and expand the 'Maps Configuration' tab to configure them.")
-            with st.beta_expander("Maps Configuration"):
+            with st.expander("Maps Configuration"):
                 self.config_dict["maps"] = []
                 for i in range(self.config_dict["maps_number"]):
                     st.write("Map number {}".format(i + 1))
-                    self.config_dict["maps"].append(MapConfigWidget(i, self.groups_df["id"]))
-
-        self.config_dict["machine_learning"] = st.checkbox("Trigger Machine Learning Prediction", False)
-        st.caption("This enables the Machine Learning section (Not implemented yet).")
+                    self.config_dict["maps"].append(MapConfigWidget(i, st.session_state["groups_df"]["id"]))
 
     def execute(self) -> None:
         """
@@ -61,7 +67,6 @@ class WebApp:
         if execute_button:
             self.raw_data_visualization()
             self.map_visualization()
-            self.improvement_prediction()
 
     def update_data_button(self) -> None:
         """
@@ -78,16 +83,16 @@ class WebApp:
         """
         groups = load_data(data_path="groups")
         latencies = load_data(data_path="latencies")
-        self.groups_df = pd.DataFrame.from_dict(groups["nodes"])
-        self.latencies_df = pd.DataFrame.from_dict(latencies["matrix"])
+        st.session_state["groups_df"] = pd.DataFrame.from_dict(groups["nodes"])
+        st.session_state["latencies_df"] = pd.DataFrame.from_dict(latencies["matrix"])
 
-        countries_list = [x for x in self.groups_df["country"]]
+        countries_list = [x for x in st.session_state["groups_df"]["country"]]
         new_cities_list = []
-        for i, city in enumerate([x for x in self.groups_df["city"]]):
+        for i, city in enumerate([x for x in st.session_state["groups_df"]["city"]]):
             if city is None:
                 city = get_capital_city(countries_list[i])
             new_cities_list.append(city)
-        self.groups_df["city"] = new_cities_list
+        st.session_state["groups_df"]["city"] = new_cities_list
 
     def raw_data_visualization(self) -> None:
         """
@@ -95,7 +100,7 @@ class WebApp:
         """
         if self.config_dict["checkbox_raw"]:
             st.header("Raw Data Visualization")
-            st.write(self.latencies_df)
+            st.write(st.session_state["latencies_df"])
 
     def map_visualization(self) -> None:
         """
@@ -107,9 +112,9 @@ class WebApp:
                 st.write("Map Number {}:".format(i + 1))
 
                 if map_config.map_type != "ArcMap":
-                    local_group_df = map_preprocessing(self.groups_df.copy(), self.latencies_df.copy(), arc_map=False)
+                    local_group_df = map_preprocessing(st.session_state["groups_df"].copy(), st.session_state["latencies_df"].copy(), arc_map=False)
                 else:
-                    arc_df = map_preprocessing(self.groups_df.copy(), self.latencies_df.copy(), id_selection=map_config.id_selection, arc_map=True)
+                    arc_df = map_preprocessing(st.session_state["groups_df"].copy(), st.session_state["latencies_df"].copy(), id_selection=map_config.id_selection, arc_map=True)
 
                 if map_config.data_type == "Deployments":
                     if map_config.map_type == "HeatMap":
@@ -119,14 +124,6 @@ class WebApp:
 
                 elif map_config.data_type == "Latencies":
                     generate_arc_map(arc_df, 0, 0, 1, map_config.min_value, map_config.max_value, map_config.color_value)
-
-    def improvement_prediction(self) -> None:
-        """
-        Section to show Machine Learning Models outputs.
-        """
-        if self.config_dict["machine_learning"]:
-            st.header("ML Modelling")
-            st.write("here it goes my model")
 
 
 # MAIN
